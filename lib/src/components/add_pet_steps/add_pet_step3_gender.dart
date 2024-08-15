19d0aaece8416a9812ba68228fb07709f6b44cb8
patrykkrzemien <Patryk.krzemien@icloud.com>
2024-08-15 11:53:58 +0200

    add_pet_step_03

--- lib/src/components/add_pet_steps/add_pet_step3_gender.dart
+++ lib/src/components/add_pet_steps/add_pet_step3_gender.dart
@@ -4,105 +4,169 @@ import 'package:pet_diary/src/components/add_pet_steps/add_pet_step4_breed.dart'
 import 'package:pet_diary/src/components/add_pet_steps/add_pet_app_bar.dart';
 import 'package:pet_diary/src/components/add_pet_steps/add_pet_segment_progress_bar.dart';
 
-class AddPetStep3Gender extends StatelessWidget {
+class AddPetStep3Gender extends StatefulWidget {
   final WidgetRef ref;
   final String petName;
   final String petAge;
-  const AddPetStep3Gender(
-      {super.key,
-      required this.ref,
-      required this.petName,
-      required this.petAge});
+
+  const AddPetStep3Gender({
+    super.key,
+    required this.ref,
+    required this.petName,
+    required this.petAge,
+  });
+
+  @override
+  _AddPetStep3GenderState createState() => _AddPetStep3GenderState();
+}
+
+class _AddPetStep3GenderState extends State<AddPetStep3Gender> {
+  bool _showContainer = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Opóźnienie w celu wywołania animacji po załadowaniu ekranu
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _showContainer = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     TextEditingController petGenderController = TextEditingController();
 
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: addPetAppBar(context, showCloseButton: true),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      AddPetSegmentProgressBar(
-                        totalSegments: 5,
-                        filledSegments: 3,
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        fillColor: const Color(0xffdfd785).withOpacity(0.7),
-                      ),
-                      const SizedBox(
-                        height: 150,
-                      ),
-                      const Text(
-                        'Please select gender',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const Text(
-                        'You can change it leter.',
-                      ),
-                      const SizedBox(height: 40),
-                      SizedBox(
-                        height: 60,
-                        width: 200,
-                        child: DropdownButtonFormField<String>(
-                          items: ['Male', 'Female'].map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value,
-                                  style: TextStyle(
-                                      color:
-                                          Theme.of(context).primaryColorDark)),
-                            );
-                          }).toList(),
-                          onChanged: (newValue) {
-                            petGenderController.text = newValue!;
-                          },
-                          decoration: const InputDecoration(
-                            labelText: 'Select Pet Gender',
-                            border: OutlineInputBorder(),
+          Column(
+            children: [
+              Container(
+                color: Theme.of(context).colorScheme.primary,
+                child: Column(
+                  children: [
+                    Divider(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    AddPetSegmentProgressBar(
+                      totalSegments: 5,
+                      filledSegments: 3,
+                      backgroundColor: Theme.of(context).colorScheme.surface,
+                      fillColor: const Color(0xffdfd785),
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 2500),
+                curve: Curves.easeOut,
+                padding: EdgeInsets.only(
+                  top: _showContainer ? 150 : 200, // Zmienia pozycję na ekranie
+                  left: 20,
+                  right: 20,
+                ),
+                child: AnimatedOpacity(
+                  opacity:
+                      _showContainer ? 1.0 : 0.0, // Zmienia przezroczystość
+                  duration: const Duration(milliseconds: 1200),
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          child: Text(
+                            'Please select gender',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 40,
-                    width: 300,
-                    child: FloatingActionButton.extended(
-                      onPressed: () {
-                        if (petGenderController.text.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('Please select pet gender.')),
-                          );
-                          return;
-                        }
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (_) => AddPetStep4Breed(
-                            ref: ref,
-                            petName: petName,
-                            petAge: petAge,
-                            petGender: petGenderController.text,
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 6.0),
+                          child: Text(
+                            'You can change it later in pet settings.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 11,
+                            ),
                           ),
-                        ));
-                      },
-                      label: Text('Next',
-                          style: TextStyle(
-                              color: Theme.of(context).primaryColorDark,
-                              fontSize: 16)),
-                      backgroundColor: const Color(0xff68a2b6).withOpacity(0.7),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 30.0),
+                          child: SizedBox(
+                            height: 60,
+                            width:
+                                300, // Dopasowano szerokość do poprzednich ekranów
+                            child: DropdownButtonFormField<String>(
+                              items: ['Male', 'Female'].map((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value,
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .primaryColorDark)),
+                                );
+                              }).toList(),
+                              onChanged: (newValue) {
+                                petGenderController.text = newValue!;
+                              },
+                              decoration: const InputDecoration(
+                                labelText: 'Select Pet Gender',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: SizedBox(
+              height: 40,
+              width: 300,
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  if (petGenderController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Please select pet gender.')),
+                    );
+                    return;
+                  }
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => AddPetStep4Breed(
+                      ref: widget.ref,
+                      petName: widget.petName,
+                      petAge: widget.petAge,
+                      petGender: petGenderController.text,
+                    ),
+                  ));
+                },
+                label: Text('Next',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColorDark,
+                        fontSize: 16)),
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
               ),
             ),
           ),
