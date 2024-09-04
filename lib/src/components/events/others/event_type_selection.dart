e9430d5c5b19424d8fe1c55695ce0850c8107ab4
Patryk <65673390+PyDeKa@users.noreply.github.com>
2024-09-04 18:31:02 +0200

    events_care (#24)

--- lib/src/components/events/others/event_type_selection.dart
+++ lib/src/components/events/others/event_type_selection.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pet_diary/src/components/events/event_care/event_type_card_care.dart';
 import 'package:pet_diary/src/components/events/event_temperature/event_type_card_temperature.dart';
-import 'package:pet_diary/src/components/events/others/event_temperature.dart';
 import 'package:pet_diary/src/components/events/event_notes/event_type_card_notes.dart';
 import 'package:pet_diary/src/components/events/event_food/functions/food_or_water_alert_dialog.dart';
+import 'package:pet_diary/src/components/events/others/event_type_card.dart';
 
 void eventTypeSelection(BuildContext context, WidgetRef ref, String petId) {
   var titleController = TextEditingController();
   var contentTextController = TextEditingController();
   var temperatureController = TextEditingController();
+  var dateController =
+      TextEditingController(); // Dodano kontroler dla opieki (Care)
 
   showModalBottomSheet(
     context: context,
@@ -71,10 +74,8 @@ void eventTypeSelection(BuildContext context, WidgetRef ref, String petId) {
                       }),
                       eventTypeCardTemperature(
                           context, temperatureController, ref, petId),
-                      eventTypeCard(context, 'Grooming',
-                          'assets/images/health_event_card/hair_brush.png', () {
-                        // TODO: Implement navigation to Grooming screen
-                      }),
+                      eventTypeCardCare(context, ref, petId,
+                          dateController), // Dodano kartę Care
                       eventTypeCard(context, 'Mood & Mental',
                           'assets/images/health_event_card/dog_love.png', () {
                         // TODO: Implement navigation to Mood & Mental Health screen
@@ -89,7 +90,7 @@ void eventTypeSelection(BuildContext context, WidgetRef ref, String petId) {
                       }),
                       eventTypeCard(context, 'Others',
                           'assets/images/health_event_card/others.png', () {
-                        // TODO: Implement navigation to Medications & Vaccines screen
+                        // TODO: Implement navigation to Others screen
                       }),
                     ],
                   ),
@@ -100,47 +101,5 @@ void eventTypeSelection(BuildContext context, WidgetRef ref, String petId) {
         },
       );
     },
-  );
-}
-
-Widget eventTypeCard(
-    BuildContext context, String title, String imagePath, VoidCallback onTap) {
-  return GestureDetector(
-    onTap: onTap,
-    child: Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 120,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-              ),
-              image: DecorationImage(
-                image: AssetImage(imagePath),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 13.0, left: 5, right: 5),
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).primaryColorDark,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-    ),
   );
 }
