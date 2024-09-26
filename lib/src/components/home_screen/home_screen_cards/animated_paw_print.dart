bbc3ed91acca3403ec9aba4f84b388cf837e262d
Damian Krzemien <damian.krzem@gmail.com>
2024-09-26 15:15:08 +0200

    update animation

--- lib/src/components/home_screen/home_screen_cards/animated_paw_print.dart
+++ lib/src/components/home_screen/home_screen_cards/animated_paw_print.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class AnimatedPawPrint extends StatefulWidget {
   final bool isTodayExtended;
@@ -18,22 +17,28 @@ class AnimatedPawPrint extends StatefulWidget {
 class _AnimatedPawPrintState extends State<AnimatedPawPrint>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _fadeInOut;
+  late List<Animation<double>> _fadeAnimations;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
+      duration: const Duration(seconds: 4), // Total duration for all animations
       vsync: this,
-    )..repeat(reverse: true);
+    )..repeat(); // Continuous repeat without reverse
 
-    _fadeInOut = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeInOut,
-      ),
-    );
+    _fadeAnimations = List.generate(4, (index) {
+      return Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+          parent: _controller,
+          curve: Interval(
+            index / 4, // Start fading in
+            (index + 0.5) / 4, // Halfway through fade out
+            curve: Curves.easeInOut, // Smooth transition
+          ),
+        ),
+      );
+    });
   }
 
   @override
@@ -44,49 +49,74 @@ class _AnimatedPawPrintState extends State<AnimatedPawPrint>
 
   @override
   Widget build(BuildContext context) {
-    final backgroundColor = Theme.of(context).colorScheme.primary;
     final pawColor = Theme.of(context).primaryColorDark;
 
     return Container(
       width: 50,
       height: 50,
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        shape: BoxShape.circle,
-      ),
-      padding: const EdgeInsets.all(8),
       child: Opacity(
         opacity: widget.isTodayExtended ? 1.0 : 0.3,
         child: widget.strike == 0
-            ? SvgPicture.asset(
-                'assets/images/svg/paws1.svg',
+            ? Image.asset(
+                'assets/images/others/paw.png',
                 width: 25,
                 height: 25,
                 color: pawColor,
               )
             : Stack(
-                alignment: Alignment.center,
                 children: [
-                  Align(
-                    alignment: Alignment.center,
+                  // Paw print 1a
+                  Positioned(
+                    top: 45,
+                    left: 5,
                     child: FadeTransition(
-                      opacity: _fadeInOut,
-                      child: SvgPicture.asset(
-                        'assets/images/svg/paws1.svg',
-                        width: 30,
-                        height: 30,
+                      opacity: _fadeAnimations[0],
+                      child: Image.asset(
+                        'assets/images/others/paw.png',
+                        width: 20,
+                        height: 20,
                         color: pawColor,
                       ),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.center,
+                  // Paw print 1b
+                  Positioned(
+                    top: 30,
+                    left: 15,
                     child: FadeTransition(
-                      opacity: ReverseAnimation(_fadeInOut),
-                      child: SvgPicture.asset(
-                        'assets/images/svg/paws2.svg',
-                        width: 30,
-                        height: 30,
+                      opacity: _fadeAnimations[1],
+                      child: Image.asset(
+                        'assets/images/others/paw.png',
+                        width: 20,
+                        height: 20,
+                        color: pawColor,
+                      ),
+                    ),
+                  ),
+                  // Paw print 2a
+                  Positioned(
+                    top: 15,
+                    left: 5,
+                    child: FadeTransition(
+                      opacity: _fadeAnimations[2],
+                      child: Image.asset(
+                        'assets/images/others/paw.png',
+                        width: 20,
+                        height: 20,
+                        color: pawColor,
+                      ),
+                    ),
+                  ),
+                  // Paw print 2b
+                  Positioned(
+                    top: 0,
+                    left: 15,
+                    child: FadeTransition(
+                      opacity: _fadeAnimations[3],
+                      child: Image.asset(
+                        'assets/images/others/paw.png',
+                        width: 20,
+                        height: 20,
                         color: pawColor,
                       ),
                     ),
